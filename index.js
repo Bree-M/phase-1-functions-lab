5566518de6bc8e0c3ce82ea840bb7eaa1d22e41a
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-09 19:08:33 +0300

    commit 2

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const hqLocation = 42;
 return Math.abs(pickupLocation - hqLocation);
 }
 
+function distanceFromHqInFeet(pickupLocation) {
+   return Math.abs(pickupLocation - 42);
+
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
